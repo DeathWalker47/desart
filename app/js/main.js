43cc405018373c70d01859d6c558f4c5706032f8
DeathWalker47 <fizer_00@mail.ru>
2021-10-17 22:36:25 +0300

    почти Сделал весь лендинг,осталось слайдер и адаптив

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,18 @@
 $(function(){
   
 });
+
+let center = [56.13821608478914,47.274704757949756];
+
+function init() {
+	let map = new ymaps.Map('map', {
+		center: center,
+		zoom: 16
+	});
+
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+  map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
+}
+
+ymaps.ready(init);

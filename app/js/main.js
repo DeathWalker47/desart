7c71519f6eabbc93e400db9bca03ae824dc22fed
DeathWalker47 <fizer_00@mail.ru>
2021-10-18 22:19:38 +0300

    Сделал весь лендос + подключил анимацию

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,22 @@
 $(function(){
-  
+	
+    $(".menu__link, .logo").on("click", function (e) {
+      e.preventDefault();
+      var id = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({
+          scrollTop: top
+      }, 1500);
+  });
+
+});
+
+AOS.init({
+	easing: "linear",
+	disable: 'mobile',
+	duration: 800,
+	offset: 200,
+
 });
 
 let center = [56.13821608478914,47.274704757949756];
@@ -16,3 +33,27 @@ function init() {
 }
 
 ymaps.ready(init);
+
+new Swiper('.team-slider',{
+  loop:true,
+  
+  slidesPerView: 5,
+  speed: 600,
+	spaceBetween: 30,
+	grabCurcor: true,
+	keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+	// mousewheel: {
+  //   invert: true,
+  // },
+	autoplay: {
+		delay: 8000,
+	},
+	slidesPerGroup: 1,
+	pagination: {
+    el: '.swiper-pagination',
+		clickable: true,
+  },
+ });
